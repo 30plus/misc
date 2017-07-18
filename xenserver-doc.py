75e75e343a8d24270f446fab2afdd112d107440c
Xingyou Chen <niatlantice@gmail.com>
2017-07-18 11:24:13 +0800

    Added script downloading XenServer docs

---
+++ xenserver-doc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Clone openstack and keep the local copy up-to-date
+
+import pycurl,os
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+buf = BytesIO()
+src_root = os.getcwd()
+
+curl = pycurl.Curl()
+curl.setopt(curl.URL, 'https://docs.citrix.com/en-us/xenserver/current-release.html')
+curl.setopt(curl.WRITEDATA, buf)
+curl.perform()
+curl.close()
+
+content = buf.getvalue()
+soup = BeautifulSoup(content, 'html.parser')
+
+def has_class(tag):
+	return tag.name == 'div' and tag.has_attr('class') and tag['class'][0] == 'dl-doc-downloads'
+
+update_log = open('/tmp/update.log', 'w')
+for tag in soup.find_all(has_class):
+	doc_file = tag.a['href']
+	print(doc_file[doc_file.rindex('/')+1:])
+	os.system('wget -nv -c https://docs.citrix.com' + doc_file)
+
+update_log.close()
